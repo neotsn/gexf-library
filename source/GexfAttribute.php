79466eba97704311e68bef63cd192960b55af3ff
neotsn neotsn <neotsn@gmail.com>
2020-04-29 14:32:46 -0500

    Work for #4 "Allow explicit Attribute ID specification" - Cleanse explicit id before storage
    Signed-off-by: neotsn neotsn <neotsn@gmail.com>

--- source/GexfAttribute.php
+++ source/GexfAttribute.php
@@ -348,7 +348,7 @@ class GexfAttribute
      */
     private function setId($forcedId = null)
     {
-        $this->id = (isset($forcedId)) ? $forcedId : 'a-' . md5($this->getName());
+        $this->id = (isset($forcedId)) ? Gexf::cleanseId($forcedId) : 'a-' . md5($this->getName());
 
         return $this;
     }
